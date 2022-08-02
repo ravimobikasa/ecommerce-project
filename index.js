69c637ba1adaf87443e5a745cf5eb6a457dd46ce
shubh19S <shubhamsaini@mobikasa.com>
2022-08-02 18:40:03 +0530

    database localhost connection setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const db = require("./config/db");
+
+dotenv.config();
+
+
+
+app.use(express.json())
+
+// Routes
+
+
+
+
+db.sync()
+  .then((result) => console.log("sync success"))
+  .catch((err) => console.log("sync error ", err.message));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
