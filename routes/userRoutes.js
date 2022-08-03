25dabb8380ef53b214d06fe4cf7179130523df4d
shubh19S <shubhamsaini@mobikasa.com>
2022-08-03 19:47:55 +0530

    register with views

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,8 +4,20 @@ const validate = require('./../middleware/validate')
 const userValidation = require('./../validations/userValidation')
 const userController = require('./../controllers/userController')
 
-router.route('/').post(validate(userValidation.registerUser), userController.registerUser)
+router.route('/register').get((req, res) => {
+  res.render('register')
+})
 
-// router.route('/login').post(validate(userValidation.login), userController.loginUser)
+router.route('/register').post(validate(userValidation.registerUser), userController.registerUser)
+
+router.route('/login').get((req, res) => {
+  res.render('login')
+})
+
+router.route('/login').post(validate(userValidation.login), userController.login)
+
+router.route('/test').get((req, res) => {
+  res.render('test')
+})
 
 module.exports = router
