fe70aef79af2c61125266897f709d3cd07bebee0
“Ravindra” <ravindra@mobikasa.com>
2022-08-05 14:33:51 +0530

    conflict resolve

--- models/index.js
+++ models/index.js
@@ -1,17 +1,14 @@
 const User = require('./user')
 const Address = require('./address')
 const OrderDetail = require('./orderDetail')
-<<<<<<< HEAD
 const OrderItem = require('./orderItem')
 const Product = require('./products')
 
 /**
  * Relationship mapping.
  * */
-=======
 const Cart = require('./cart')
 const Products = require('./products')
->>>>>>> 483db6f72e78b3b9fcb65d6f3bf71c143f558b53
 
 User.hasMany(OrderDetail)
 User.hasMany(Address)
