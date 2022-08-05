b1869c75ccfa68a5f7187d36baa932a16a7abf05
“Ravindra” <ravindra@mobikasa.com>
2022-08-05 14:40:39 +0530

    conflict resolved

--- models/products.js
+++ models/products.js
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-'use strict'
-const { DataTypes, Model } = require('sequelize')
-const sequelize = require('../config/db')
-
-class Product extends Model {
-  /**
-   * Helper method for defining associations.
-   * This method is not a part of Sequelize lifecycle.
-   * The `models/index` file will call this method automatically.
-   */
-  static associate(models) {
-    const { OrderItem } = models
-    Product.hasMany(OrderItem)
-  }
-}
-
-Product.init(
-  {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    imageUrl: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-  }
-)
-
-module.exports = Product
-=======
 'use strict'
 const { DataTypes, Model } = require('sequelize')
 const sequelize = require('../config/db')
@@ -75,4 +32,3 @@ Products.init(
   }
 )
 module.exports = Products
->>>>>>> 483db6f72e78b3b9fcb65d6f3bf71c143f558b53
