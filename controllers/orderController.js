e110e7932bc10da7a3546e97c7df74587ad86ac8
“Ravindra” <ravindra@mobikasa.com>
2022-08-23 17:07:31 +0530

    Added My order sorted

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,73 +1,73 @@
-const { orderService } = require('../services')
-const { Cart, OrderDetail, Product, User, OrderItem } = require('../models')
-const stripe = require('../payment/stripe')
-const { Op } = require('sequelize')
+const { orderService } = require("../services");
+const { Cart, OrderDetail, Product, User, OrderItem } = require("../models");
+const stripe = require("../payment/stripe");
+const { Op } = require("sequelize");
 
 const stripeWebHook = async (req, res) => {
-  const endpointSecret = process.env.END_POINT_SECRET
+  const endpointSecret = process.env.END_POINT_SECRET;
 
-  const signature = req.headers['stripe-signature']
+  const signature = req.headers["stripe-signature"];
   try {
     // verifying  stripe payload with endpointSecret
-    let event = stripe.webhooks.constructEvent(req.body, signature, endpointSecret)
+    let event = stripe.webhooks.constructEvent(req.body, signature, endpointSecret);
 
     switch (event.type) {
-      case 'payment_intent.succeeded': {
-        const session = event.data.object
-        await orderService.updateOrderPaymentStatus(session, 'CONFIRMED')
-        break
+      case "payment_intent.succeeded": {
+        const session = event.data.object;
+        await orderService.updateOrderPaymentStatus(session, "CONFIRMED");
+        break;
       }
 
-      case 'payment_intent.payment_failed': {
-        const session = event.data.object
-        console.log('payment_intent.payment_failed', session)
-        await orderService.updateOrderPaymentStatus(session, 'FAILED')
-        break
+      case "payment_intent.payment_failed": {
+        const session = event.data.object;
+        console.log("payment_intent.payment_failed", session);
+        await orderService.updateOrderPaymentStatus(session, "FAILED");
+        break;
       }
 
-      case 'checkout.session.completed': {
-        const session = event.data.object
-        const customer = await stripe.customers.retrieve(session.customer)
+      case "checkout.session.completed": {
+        const session = event.data.object;
+        const customer = await stripe.customers.retrieve(session.customer);
 
-        await orderService.updateOrderBySession(session)
+        await orderService.updateOrderBySession(session);
 
         // clearing the cart when order  success.
-        await Cart.destroy({ where: { userId: customer.metadata.userId } })
+        await Cart.destroy({ where: { userId: customer.metadata.userId } });
       }
 
       // handling expired session when user has not made any payment until 24 hours.
-      case 'checkout.session.expired': {
-        const session = event.data.object
-        const order = await OrderDetail.findOne({ where: { stripeSessionId: session.id } })
+      case "checkout.session.expired": {
+        const session = event.data.object;
+        const order = await OrderDetail.findOne({ where: { stripeSessionId: session.id } });
 
-        if (order && order.orderStatus === 'PENDING') {
-          await order.update({ orderStatus: 'FAILED' })
+        if (order && order.orderStatus === "PENDING") {
+          await order.update({ orderStatus: "FAILED" });
         }
-        break
+        break;
       }
       default:
-        console.log(`Unhandled event type ${event.type}`)
+        console.log(`Unhandled event type ${event.type}`);
     }
 
-    res.send()
+    res.send();
   } catch (err) {
-    console.log(err)
-    res.status(400).send(`Webhook Error: ${err?.message}`)
+    console.log(err);
+    res.status(400).send(`Webhook Error: ${err?.message}`);
   }
-}
+};
 
 const createCheckoutSession = async (req, res) => {
-  const currency = 'inr'
-  const allowedCountryISOCodes = ['IN']
-  const userId = req.user.id
+  const currency = "inr";
+  const allowedCountryISOCodes = ["IN"];
+  const userId = req.user.id;
   try {
-    const user = await User.findByPk(userId)
+    const user = await User.findByPk(userId);
     const customer = await stripe.customers.create({
       email: user.email,
-      metadata: { userId },
-    })
+      metadata: { userId }
+    });
 
-    const { orderDetail, orderItemsResult } = await orderService.createOrder(userId)
+    const { orderDetail, orderItemsResult } = await orderService.createOrder(userId);
 
     const stripeLineItems = orderItemsResult.map((item) => {
       return {
@@ -75,175 +75,172 @@ const createCheckoutSession = async (req, res) => {
           currency: currency,
           product_data: {
             name: item.productPrice,
-            images: [item.productImage || ''],
+            images: [item.productImage || ""],
             // description: item.productDescription || 'N/A',
             metadata: {
               id: item.productId,
-              price: item.productPrice,
-            },
+              price: item.productPrice
+            }
           },
-          unit_amount: item.productPrice * 100,
+          unit_amount: item.productPrice * 100
         },
-        quantity: item.quantity,
-      }
-    })
+        quantity: item.quantity
+      };
+    });
 
-    const successURL = `${req.protocol}://${req.get('host')}/order/success?session_id={CHECKOUT_SESSION_ID}&orderId=${
+    const successURL = `${req.protocol}://${req.get("host")}/order/success?session_id={CHECKOUT_SESSION_ID}&orderId=${
       orderDetail.id
-    }&paymentStatus=SUCCESS`
+    }&paymentStatus=SUCCESS`;
 
-    const cancelURL = `${req.protocol}://${req.get('host')}/order/failure?session_id={CHECKOUT_SESSION_ID}&orderId=${
+    const cancelURL = `${req.protocol}://${req.get("host")}/order/failure?session_id={CHECKOUT_SESSION_ID}&orderId=${
       orderDetail.id
-    }&paymentStatus=FAILED`
+    }&paymentStatus=FAILED`;
 
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      mode: 'payment',
+      payment_method_types: ["card"],
+      mode: "payment",
       currency: currency,
       line_items: stripeLineItems,
       shipping_address_collection: {
-        allowed_countries: allowedCountryISOCodes,
+        allowed_countries: allowedCountryISOCodes
       },
-      expand: ['line_items'],
+      expand: ["line_items"],
       customer: customer.id,
       success_url: successURL,
-      cancel_url: cancelURL,
-    })
+      cancel_url: cancelURL
+    });
 
-    await orderDetail.update({ stripeSessionId: session.id })
+    await orderDetail.update({ stripeSessionId: session.id });
 
-    res.redirect(303, session.url)
+    res.redirect(303, session.url);
   } catch (err) {
-    console.log(err)
-    res.json({ error: err })
+    console.log(err);
+    res.json({ error: err });
   }
-}
+};
 
 const getAllOrders = async (req, res) => {
-  let { limit, page, search } = req.query
+  let { limit, page, search } = req.query;
 
   try {
-    limit = parseInt(limit) || 12
-    page = parseInt(page) || 1
-    let _search = search || ''
-    page = Math.abs(page)
-    let offset = page * limit - limit
-    let query
-    let count
+    limit = parseInt(limit) || 12;
+    page = parseInt(page) || 1;
+    let _search = search || "";
+    page = Math.abs(page);
+    let offset = page * limit - limit;
+    let query;
+    let count;
     if (!search) {
       query = {
-        order: [['createdAt', 'DESC']],
+        order: [["createdAt", "DESC"]],
         limit: Math.abs(limit),
-        offset: offset,
-      }
-      count = await OrderDetail.count()
+        offset: offset
+      };
+      count = await OrderDetail.count();
     }
     if (search) {
       query = {
         where: {
           [Op.or]: {
             userFirstName: {
-              [Op.substring]: `${_search}`,
+              [Op.substring]: `${_search}`
             },
             userLastName: {
-              [Op.substring]: `${_search}`,
+              [Op.substring]: `${_search}`
             },
             id: `${_search}`,
-            orderStatus: `${_search}`,
-          },
+            orderStatus: `${_search}`
+          }
         },
         limit: Math.abs(limit),
         offset: offset,
-        order: [['createdAt', 'DESC']],
-      }
+        order: [["createdAt", "DESC"]]
+      };
     }
     count = await OrderDetail.count({
       where: {
         [Op.or]: {
           userFirstName: {
-            [Op.substring]: `${_search}`,
+            [Op.substring]: `${_search}`
           },
           userLastName: {
-            [Op.substring]: `${_search}`,
+            [Op.substring]: `${_search}`
           },
           id: `${_search}`,
-          orderStatus: `${_search}`,
-        },
-      },
-    })
+          orderStatus: `${_search}`
+        }
+      }
+    });
 
-    const orders = await OrderDetail.findAll(query)
-    res.render('orders', { orders, pagination: { count, limit, page, search } })
+    const orders = await OrderDetail.findAll(query);
+    res.render("orders", { orders, pagination: { count, limit, page, search } });
   } catch (err) {
-    console.log(err)
-    res.render('500error')
+    console.log(err);
+    res.render("500error");
   }
-}
+};
 
 const getOrder = async (req, res) => {
-  const { orderId } = req.params
+  const { orderId } = req.params;
 
   const order = await OrderDetail.findByPk(orderId, {
-    include: [OrderItem],
-  })
+    include: [OrderItem]
+  });
 
   if (!order) {
-    return res.render('404error', { errorMessage: `Order Not Found` })
+    return res.render("404error", { errorMessage: `Order Not Found` });
   }
-  res.render('orderDetail', { order })
-}
+  res.render("orderDetail", { order });
+};
 
 const orderPaymentStatus = async (req, res) => {
-  const { paymentStatus, session_id } = req.query
+  const { paymentStatus, session_id } = req.query;
 
-  let order = await OrderDetail.findOne({ where: { stripeSessionId: session_id }, include: [OrderItem] })
+  let order = await OrderDetail.findOne({ where: { stripeSessionId: session_id }, include: [OrderItem] });
 
   if (!order) {
-    return res.redirect('/cart')
+    return res.redirect("/cart");
   }
 
-  let message
+  let message;
 
-  if (paymentStatus.toLowerCase() === 'success') {
-    message = 'Your order has been placed successfully'
-  } else if (paymentStatus.toLowerCase() === 'failed') {
-    const session = await stripe.checkout.sessions.retrieve(session_id)
+  if (paymentStatus.toLowerCase() === "success") {
+    message = "Your order has been placed successfully";
+  } else if (paymentStatus.toLowerCase() === "failed") {
+    const session = await stripe.checkout.sessions.retrieve(session_id);
 
-    if (order && session && session.status === 'open') {
-      await stripe.checkout.sessions.expire(session_id)
-      order = await order.update({ orderStatus: 'FAILED' })
+    if (order && session && session.status === "open") {
+      await stripe.checkout.sessions.expire(session_id);
+      order = await order.update({ orderStatus: "FAILED" });
     }
-    message = 'Your order has been failed'
+    message = "Your order has been failed";
   }
 
-  res.render('orderDetail', { order, message })
-}
+  res.render("orderDetail", { order, message });
+};
 
 const getMyOrders = async (req, res) => {
-  let { limit, page, search } = req.query
+  let { limit, page, search } = req.query;
 
   try {
-    const userId = req.user.id
-    limit = parseInt(limit) || 12
-    page = parseInt(page) || 1
-    let _search = search || ''
-    page = Math.abs(page)
-    let offset = page * limit - limit
-    let query
-    let count
+    const userId = req.user.id;
+    limit = parseInt(limit) || 12;
+    page = parseInt(page) || 1;
+    let _search = search || "";
+    page = Math.abs(page);
+    let offset = page * limit - limit;
+    let query;
+    let count;
     if (!search) {
       query = {
         where: {
-          userId,
+          userId
         },
+        order: [["createdAt", "DESC"]],
         limit: Math.abs(limit),
-        offset: offset,
-      }
-      count = await OrderDetail.count({
-        where: {
-          userId,
-        },
-      })
+        offset: offset
+      };
+
     }
     if (search) {
       query = {
@@ -252,116 +249,117 @@ const getMyOrders = async (req, res) => {
           [Op.or]: {
             id: `${_search}`,
             orderStatus: {
-              [Op.substring]: `${_search}`,
-            },
-          },
+              [Op.substring]: `${_search}`
+            }
+          }
         },
+        order: [["createdAt", "DESC"]],
         limit: Math.abs(limit),
-        offset: offset,
-      }
+        offset: offset
+      };
     }
     count = await OrderDetail.count({
       where: {
         userId,
         [Op.or]: {
           id: {
-            [Op.substring]: `${_search}`,
+            [Op.substring]: `${_search}`
           },
           orderStatus: {
-            [Op.substring]: `${_search}`,
-          },
-        },
-      },
-    })
-    const orders = await OrderDetail.findAll(query)
-    res.render('orders', { orders, origin: 'myOrder', pagination: { count, limit, page, search } })
+            [Op.substring]: `${_search}`
+          }
+        }
+      }
+    });
+    const orders = await OrderDetail.findAll(query);
+    res.render("orders", { orders, origin: "myOrder", pagination: { count, limit, page, search } });
   } catch (err) {
-    res.render('500error')
+    res.render("500error");
   }
-}
+};
 
 const getMyOrder = async (req, res) => {
-  const { orderId } = req.params
+  const { orderId } = req.params;
 
-  const userId = req.user.id
+  const userId = req.user.id;
 
   const order = await OrderDetail.findOne({
     where: { id: orderId, userId },
-    include: [OrderItem],
-  })
+    include: [OrderItem]
+  });
 
   if (!order) {
-    return res.render('404error', { errorMessage: `Order Not Found` })
+    return res.render("404error", { errorMessage: `Order Not Found` });
   }
-  res.render('orderDetail', { order })
-}
+  res.render("orderDetail", { order });
+};
 
 // using stripe line items for storing the carts product details. Creating order after successful payment
 const stripeWebHookOld = async (req, res) => {
-  const endpointSecret = process.env.END_POINT_SECRET
+  const endpointSecret = process.env.END_POINT_SECRET;
 
-  const signature = req.headers['stripe-signature']
+  const signature = req.headers["stripe-signature"];
   try {
-    let event = stripe.webhooks.constructEvent(req.body, signature, endpointSecret)
-    console.log(`Unhandled event type ${event.type}`)
+    let event = stripe.webhooks.constructEvent(req.body, signature, endpointSecret);
+    console.log(`Unhandled event type ${event.type}`);
 
     switch (event.type) {
-      case 'payment_intent.succeeded':
-        const session = event.data.object
-        await orderService.updateOrderPaymentStatus(session, 'CONFIRMED')
-        break
+      case "payment_intent.succeeded":
+        const session = event.data.object;
+        await orderService.updateOrderPaymentStatus(session, "CONFIRMED");
+        break;
 
-      case 'checkout.session.completed': {
-        const session = event.data.object
+      case "checkout.session.completed": {
+        const session = event.data.object;
 
-        const customer = await stripe.customers.retrieve(session.customer)
+        const customer = await stripe.customers.retrieve(session.customer);
 
         const sessionDetail = await stripe.checkout.sessions.listLineItems(session.id, {
-          expand: ['data.price.product'],
-        })
+          expand: ["data.price.product"]
+        });
 
-        await orderService.createOrder(customer.metadata.userId, session, sessionDetail)
+        await orderService.createOrder(customer.metadata.userId, session, sessionDetail);
 
-        await Cart.destroy({ where: { userId: customer.metadata.userId } })
+        await Cart.destroy({ where: { userId: customer.metadata.userId } });
 
-        break
+        break;
       }
 
-      case 'checkout.session.async_payment_failed': {
-        const session = event.data.object
-        await orderService.updateOrderPaymentStatus(session, 'FAILED')
-        break
+      case "checkout.session.async_payment_failed": {
+        const session = event.data.object;
+        await orderService.updateOrderPaymentStatus(session, "FAILED");
+        break;
       }
     }
 
-    res.send()
+    res.send();
   } catch (err) {
-    console.log(err)
-    res.status(400).send(`Webhook Error: ${err?.message}`)
+    console.log(err);
+    res.status(400).send(`Webhook Error: ${err?.message}`);
   }
-}
+};
 
 const createCheckoutSessionOld = async (req, res) => {
-  const currency = 'inr'
-  const allowedCountryISOCodes = ['IN']
-  const userId = req.user.id
+  const currency = "inr";
+  const allowedCountryISOCodes = ["IN"];
+  const userId = req.user.id;
 
   const successURL = `${req.protocol}://${req.get(
-    'host'
-  )}/order/success?session_id={CHECKOUT_SESSION_ID}&paymentStatus=SUCCESS`
+    "host"
+  )}/order/success?session_id={CHECKOUT_SESSION_ID}&paymentStatus=SUCCESS`;
   const cancelURL = `${req.protocol}://${req.get(
-    'host'
-  )}/order/failure?session_id={CHECKOUT_SESSION_ID}&paymentStatus=FAILED`
+    "host"
+  )}/order/failure?session_id={CHECKOUT_SESSION_ID}&paymentStatus=FAILED`;
 
   try {
-    const user = await User.findByPk(userId)
+    const user = await User.findByPk(userId);
 
     const customer = await stripe.customers.create({
       email: user.email,
-      metadata: { userId },
-    })
+      metadata: { userId }
+    });
 
-    const cartItems = await Cart.findAll({ where: { userId }, include: [Product] })
+    const cartItems = await Cart.findAll({ where: { userId }, include: [Product] });
 
     const stripeLineItems = cartItems.map((item) => {
       return {
@@ -369,38 +367,38 @@ const createCheckoutSessionOld = async (req, res) => {
           currency: currency,
           product_data: {
             name: item.Product.title,
-            images: [item.Product.imageUrl || ''],
-            description: item.Product.description || 'N/A',
+            images: [item.Product.imageUrl || ""],
+            description: item.Product.description || "N/A",
             metadata: {
               id: item.Product.id,
-              price: item.Product.price,
-            },
+              price: item.Product.price
+            }
           },
-          unit_amount: item.Product.price * 100,
+          unit_amount: item.Product.price * 100
         },
-        quantity: item.quantity,
-      }
-    })
+        quantity: item.quantity
+      };
+    });
 
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      mode: 'payment',
+      payment_method_types: ["card"],
+      mode: "payment",
       currency: currency,
       line_items: stripeLineItems,
       shipping_address_collection: {
-        allowed_countries: allowedCountryISOCodes,
+        allowed_countries: allowedCountryISOCodes
       },
-      expand: ['line_items'],
+      expand: ["line_items"],
       customer: customer.id,
       success_url: successURL,
-      cancel_url: cancelURL,
-    })
-    res.redirect(303, session.url)
+      cancel_url: cancelURL
+    });
+    res.redirect(303, session.url);
   } catch (err) {
-    console.log(err)
-    res.json({ error: err })
+    console.log(err);
+    res.json({ error: err });
   }
-}
+};
 
 module.exports = {
   createCheckoutSession,
@@ -409,5 +407,5 @@ module.exports = {
   getOrder,
   orderPaymentStatus,
   getMyOrders,
-  getMyOrder,
-}
+  getMyOrder
+};
